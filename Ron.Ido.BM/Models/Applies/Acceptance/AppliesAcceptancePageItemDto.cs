af9502b4ebae4cc61d49f9297f318854832026a8
john <chief.ivs@gmail.com>
2021-05-27 08:36:00 +0300

    2122 Форма заполнения заявки

--- Ron.Ido.BM/Models/Applies/Acceptance/AppliesAcceptancePageItemDto.cs
+++ Ron.Ido.BM/Models/Applies/Acceptance/AppliesAcceptancePageItemDto.cs
@@ -1,8 +1,17 @@
-﻿namespace Ron.Ido.BM.Models.Applies.Acceptance
+﻿using Ron.Ido.EM.Enums;
+
+namespace Ron.Ido.BM.Models.Applies.Acceptance
 {
     public class AppliesAcceptancePageItemDto
     {
         public string BarCode { get; set; }
 
+        public ApplyEntryFormEnum EntryFormId { get; set; }
+
+        public string CreatorFullName { get; set; }
+
+        public string OwnerFullName { get; set; }
+
+        public string Status { get; set; }
     }
 }
--- Ron.Ido.Web/Controllers/AppliesAcceptanceController.cs
+++ Ron.Ido.Web/Controllers/AppliesAcceptanceController.cs
@@ -1,5 +1,11 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Ron.Ido.BM.Commands.Applies.Acceptance;
+using Ron.Ido.BM.Models.Applies.Acceptance;
+using Ron.Ido.BM.Models.OData;
+using Ron.Ido.EM.Enums;
+using Ron.Ido.Web.Authorization;
+using System.Threading.Tasks;
 
 namespace Ron.Ido.Web.Controllers
 {
@@ -11,6 +17,14 @@ namespace Ron.Ido.Web.Controllers
         public AppliesAcceptanceController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("api/acceptance/getpage")]
+        [AuthorizedFor(PermissionEnum.APPLY_VIEW, PermissionEnum.APPLY_CREATE, PermissionEnum.APPLY_EDIT, PermissionEnum.APPLY_DEL)]
+        public async Task<ODataPage<AppliesAcceptancePageItemDto>> GetAppliesPage([FromBody] ODataRequest request)
+        {
+            return await _mediator.Send(new GetAppliesAcceptancePageCommand(request));
         }
     }
 }
