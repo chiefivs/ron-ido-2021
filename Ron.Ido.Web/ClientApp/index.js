e819dc6dca49e0455ac7a83a586a4238a5b166ff
john <chief.ivs@gmail.com>
2021-06-08 10:27:49 +0300

    2124 коррекция перед мерджем

--- Ron.Ido.Web/ClientApp/index.js
+++ Ron.Ido.Web/ClientApp/index.js
@@ -2,14 +2,12 @@
 import 'bootstrap-less/js/bootstrap.min.js';
 import * as $ from 'jquery';
 import * as ko from 'knockout';
-import * as bindings from './modules/bindings';
 import * as cmp from './components/index';
 
 
 import { App } from './app';
 
 $(() => {
-    bindings.init($, ko);
     cmp.init();
     ko.applyBindings(App.instance());
 });
