24746c2083427c06c813f1c79da1a4e3bcd56118
john <chief.ivs@gmail.com>
2021-07-12 18:34:05 +0300

    2143

---
+++ Ron.Ido.EM/Entities/EmailAttachment.cs
@@ -0,0 +1,11 @@
+﻿namespace Ron.Ido.EM.Entities
+{
+    public class EmailAttachment
+    {
+        public long EmailId { get; set; }
+
+        public long FileInfoId { get; set; }
+
+        public virtual FileInfo FileInfo { get; set; }
+    }
+}
